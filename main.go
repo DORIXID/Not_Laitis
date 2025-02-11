c2f77a2c855231bfa3d8186843f71d6dd941d26e
dorixid <rzavinnikita@gmail.com>
2025-02-11 19:19:39 +0300

    Перенос данных в MAP

--- main.go
+++ main.go
@@ -2,40 +2,48 @@ package main
 
 import "fmt"
 
-/*Задание: Расширенные структуры
+/*Работа с картами (map)
 
-  Добавь в command новое поле category string (например, "system", "network" и т. д.).
-  Реализуй фильтрацию команд по категории (функция filterByCategory(commands []command, category string)).
+  Измени хранение команд: вместо []command используй map[string]command, где ключ — имя команды.
+  Переделай addCommand, selectAllFrom, selectNFrom, чтобы они работали с картой.
 
 */
 
 //CREATE TABLE command
 type command struct {
 	category    string
-	name        string
 	args        []string
 	description string
 }
 
+/*
+
+var people = map[string]int{
+    "Tom": 1,
+    "Bob": 2,
+    "Sam": 4,
+    "Alice": 8,
+}
+fmt.Println(people)     // map[Tom:1 Bob:2 Sam:4 Alice:8]
+
+*/
+
 //MAIN
 func main() {
-	defer finish()                           //DEFER - finish
-	var commands []command = []command{}     //СОЗДЕМ, СЛАЙС С СТРУКТУРАМИ
-	commands = addCommand(commands, command{ //Создаём новую команду с помощью addCommand
+	defer finish()                                            //DEFER - finish
+	commands := make(map[string]command)                      //СОЗДЕМ, MAP С СТРУКТУРАМИ, ключем в которой служит имя команды
+	commands = addCommand(commands, "Hello, world!", command{ //Создаём новую команду с помощью addCommand
 		category:    "system",
-		name:        "Hello, world!",
 		args:        []string{"echo Hello, world!"},
 		description: "Выводит в командную строку Windows \"Hello world!\"",
 	})
-	commands = addCommand(commands, command{ //Создаём новую команду с помощью addCommand
+	commands = addCommand(commands, "dir", command{ //Создаём новую команду с помощью addCommand
 		category:    "system",
-		name:        "dir",
 		args:        []string{"dir"},
 		description: "Выводит список файлов и папок текущей директории",
 	})
-	commands = addCommand(commands, command{ //Создаём новую команду с помощью addCommand
+	commands = addCommand(commands, "Ping Google", command{ //Создаём новую команду с помощью addCommand
 		category:    "network",
-		name:        "Ping Google",
 		args:        []string{"ping google.com"},
 		description: "Проверяет доступность Google.",
 	})
@@ -44,47 +52,48 @@ func main() {
 }
 
 //ФУНКЦИЯ ВЫБОРА команды - select i from command
-func selectNFrom(cmd command) {
-	fmt.Printf("Категория команды: %v\n", cmd.category)
-	fmt.Printf("Имя команды: %v\n", cmd.name)
-	fmt.Printf("Аргументы: %v\n", cmd.args)
-	fmt.Printf("Описание: %v\n", cmd.description)
+func selectNFrom(mapp map[string]command, key string) {
+	fmt.Printf("Имя команды: %s\n", key)
+	fmt.Printf("Категория команды: %s\n", mapp[key].category)
+	fmt.Printf("Аргументы: %s\n", mapp[key].args)
+	fmt.Printf("Описание: %s\n", mapp[key].description)
 	fmt.Printf("--------------------\n")
 }
 
 //ФУНКЦИЯ ВЫБОРА ВСЕХ КОМАНД - SELECT * FROM command
-func selectAllFrom(slice []command) {
-	for i, cmd := range slice {
-		fmt.Printf("Команда №%d\n", i+1) //Индексация в командной строке
-		selectNFrom(cmd)
+func selectAllFrom(mapp map[string]command) {
+	i := 0
+	for key, _ := range mapp {
+		i++
+		fmt.Printf("Команда №%d\n", i) //Индексация в командной строке
+		selectNFrom(mapp, key)
 	}
 }
 
 //ФУНКЦИЯ фильтрации команд по категориям
-func filterByCategory(slice []command, category string) {
+func filterByCategory(mapp map[string]command, category string) {
 	fmt.Printf("\n--------------------\nФильрация по категории \"%s\":\n--------------------\n", category)
-	for _, cmd := range slice {
+	for key, cmd := range mapp {
 		if cmd.category == category {
-			selectNFrom(cmd)
+			selectNFrom(mapp, key)
 		}
 	}
 	fmt.Printf("✓-Фильтрация завершена-✓\n--------------------\n")
 }
 
-// ДОБАВЛЕНИЕ КОМАНДЫ В СЛАЙС СТРУКТУР С КОМАНДАМИ
-// берет на вход слайс и структуру со значениями, а возвращает обновлённый слайс через return и append.
-// если попробовать что то типа slice := append(slice, cmd), то создастся новый слайс, так как он получается локальный, а настоящий не поменяется.
-func addCommand(slice []command, cmd command) []command {
-	for _, existingCmd := range slice {
-		if existingCmd.name == cmd.name {
-			fmt.Printf("\n\n❌--The entered name \"%s\" for the command is already in use--❌\n\n", existingCmd.name)
-			return (slice) // Возвращаем не обновленный слайс, если обнаружено повторение(return завершает выполнение функции)
-		}
+// ДОБАВЛЕНИЕ КОМАНДЫ В КАРТУ С КОМАНДАМИ
+// берет на вход карту(map), структуру со значениями и название ключа для структуры в карте а возвращает обновлённую карту
+func addCommand(mapp map[string]command, name string, cmd command) map[string]command {
+	// Проверка на уникальность ключа
+	if _, exists := mapp[name]; exists { //Здесь в if сначала выполняется инструкция - _, exists := mapp[name] - тут _ это значние, а exists это bool значение.
+		fmt.Printf("\n\n❌--The entered name \"%s\" for the command is already in use--❌\n\n", name) //Оно указывает на то, получилась ли операция присвоения по данному ключу или нет, а потом уже смотрится на то true  или false у exists
+		return mapp                                                                                 // Возвращаем неизмененную карту, если имя уже существует
 	}
-	return append(slice, cmd) // Если все хорошо добавляем в слайс новую структуру с командой
+	mapp[name] = cmd
+	return mapp // Если все хорошо добавляем в слайс новую структуру с командой
 }
 
-// DEFER-FUNC - выводит "Вывод завершён"
+// DEFER-FUNC - выводит в терминал "Вывод завершён"
 func finish() {
 	fmt.Println("\n\n✓✓✓-Вывод завершён-✓✓✓")
 }
